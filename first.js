42b8334105a121b7a782c0149930c837a69efe65
Muhammad Shahrukh <shahrukhsanjrani125@gmail.com>
2026-07-23 20:33:42 +0500

    Java Class
    my fill Strack calculator

---
+++ first.js
@@ -0,0 +1,36 @@
+console.log("Hello World");
+
+
+let firstname = "Muhammad";
+let lastname = "Shahrukh";
+let age = "25";
+let city = "Karachi";
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(city);
+
+let firstnumber = 35;
+let secondnumber = 10;
+
+let Addition = firstnumber + secondnumber;
+let Multiplication = firstnumber * secondnumber;
+let Division = firstnumber / secondnumber;
+let Substraction  = firstnumber - secondnumber;
+let Modules = firstnumber % secondnumber;
+
+console.log("first Number :", firstnumber);
+console.log("Second Number :", secondnumber);
+
+console.log('______________');
+
+console('Addion ')
+console.log(Addition);
+console.log('Multiplication ')
+console.log(Multiplication);
+console.log('Division ')
+console.log(Division);
+console.log('Substraction ')
+console.log(Substraction);
+console.log('Modules ')
+console.log(Modules);

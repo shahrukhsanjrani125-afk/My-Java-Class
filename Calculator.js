50ed6d2652de85500fb2cf4f463f440630939931
Muhammad Shahrukh <shahrukhsanjrani125@gmail.com>
2026-07-24 22:50:17 +0500

    calculate
    calculator work

---
+++ Calculator.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+input: process.stdin,
+ output: process.stdout
+});
+
+
+rl.question("Enter the first number: ", (firstInput) => {
+        
+    rl.question("Enter the second number: ", (secondInput) => {
+
+        sk.question("Enter the first number: ", (firstInput) => {
+        
+    sk.question("Enter the second number: ", (secondInput) => {
+    let firstNumber = Number(firstInput);
+        let secondNumber = Number(secondInput);
+        let addition = firstNumber + secondNumber;
+        let subtraction = firstNumber - secondNumber;
+        let multiplication = firstNumber * secondNumber;
+        let division = firstNumber / secondNumber;
+        let modulo = firstNumber % secondNumber;
+
+        console.log("First Number  :", firstNumber);
+        console.log("Second Number :", secondNumber);
+        console.log("Addition      :", addition);
+        console.log("Subtraction   :", subtraction);
+        console.log("Multiplication:", multiplication);
+        console.log("Division      :", division);
+        console.log("Modulo        :", modulo);
+
+        rl.close(); 
+    });
+});
